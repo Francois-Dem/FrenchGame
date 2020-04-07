1809b53071ddc2d4e7e05111cf75e25becf95cb0
Francois Demichelis <francois.demichelis@gmail.com>
2020-04-07 17:52:10 +0200

    définition des class + dev du début du jeu

---
+++ FrenchGame/FrenchGame/Classes/Weapon.swift
@@ -0,0 +1,21 @@
+//
+//  Weapon.swift
+//  FrenchGame
+//
+//  Created by Noblus Mac on 30/03/2020.
+//  Copyright © 2020 FrancoisDemichelis. All rights reserved.
+//
+
+import Foundation
+
+class Weapon {
+    var damage: Int
+    var name: String
+    
+    init(damage: Int, name: String) {
+        self.damage = damage
+        self.name = name
+        
+    }
+}
+
